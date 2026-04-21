d3f27dd59a35acbb5e13aab139efc9581751e614
MintyU <mintyu.dev@gmail.com>
2026-04-21 14:23:04 +0900

    Feat: Initial Setting For Kotlin

---
+++ omos/src/main/kotlin/com/back/omos/OmosApplication.kt
@@ -0,0 +1,11 @@
+package com.back.omos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OmosApplication
+
+fun main(args: Array<String>) {
+	runApplication<OmosApplication>(*args)
+}
