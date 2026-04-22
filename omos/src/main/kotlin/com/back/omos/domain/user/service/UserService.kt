14356c0c0e90014387dc04851d3efff4765a663b
MintyU <mintyu.dev@gmail.com>
2026-04-22 11:01:49 +0900

    Feat: DTO 생성, UserService 구현

---
+++ omos/src/main/kotlin/com/back/omos/domain/user/service/UserService.kt
@@ -0,0 +1,41 @@
+package com.back.omos.domain.user.service
+
+import com.back.omos.domain.user.dto.UserCreateReq
+import com.back.omos.domain.user.dto.UserRes
+
+/**
+ * 사용자 정보 관리를 담당하는 서비스 인터페이스입니다.
+ *
+ * <p>
+ * 사용자 가입, 조회, 정보 수정 등의 비즈니스 로직을 정의합니다.
+ *
+ * @author MintyU
+ * @since 2026-04-22
+ */
+interface UserService {
+    /**
+     * 새로운 사용자를 생성(등록)합니다.
+     *
+     * @param request 신규 사용자 생성 정보를 담은 DTO
+     * @return 생성된 사용자 정보 DTO
+     */
+    fun createUser(request: UserCreateReq): UserRes
+
+    /**
+     * GitHub 고유 ID를 통해 사용자 정보를 조회합니다.
+     *
+     * @param githubId 조회할 GitHub 고유 ID
+     * @return 조회된 사용자 정보 DTO
+     */
+    fun getUserByGithubId(githubId: String): UserRes
+
+    /**
+     * 사용자의 이름과 이메일 정보를 업데이트합니다.
+     *
+     * @param githubId 업데이트할 사용자의 GitHub 고유 ID
+     * @param name 변경할 이름 (null인 경우 변경 없음)
+     * @param email 변경할 이메일 (null인 경우 변경 없음)
+     * @return 업데이트된 사용자 정보 DTO
+     */
+    fun updateProfile(githubId: String, name: String?, email: String?): UserRes
+}
---
+++ omos/src/main/kotlin/com/back/omos/domain/user/service/UserServiceImpl.kt
@@ -0,0 +1,53 @@
+package com.back.omos.domain.user.service
+
+import com.back.omos.domain.user.dto.UserCreateReq
+import com.back.omos.domain.user.dto.UserRes
+import com.back.omos.domain.user.repository.UserRepository
+import com.plog.global.exception.errorCode.AuthErrorCode
+import com.plog.global.exception.exceptions.AuthException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+/**
+ * [UserService]의 구현체로, 사용자의 비즈니스 로직을 처리합니다.
+ *
+ * <p>
+ * [UserRepository]를 주입받아 데이터베이스에 접근하며,
+ * 사용자가 존재하지 않거나 생성 실패 시 [AuthException]을 발생시킵니다.
+ *
+ * @property userRepository 사용자 데이터 액세스를 위한 Repository
+ *
+ * @author MintyU
+ * @since 2026-04-22
+ * @see UserService
+ */
+@Service
+@Transactional(readOnly = true)
+class UserServiceImpl(
+    private val userRepository: UserRepository
+) : UserService {
+
+    @Transactional
+    override fun createUser(request: UserCreateReq): UserRes {
+        if (userRepository.existsByGithubId(request.githubId)) {
+            throw AuthException(AuthErrorCode.USER_ALREADY_EXIST)
+        }
+        val user = userRepository.save(request.toEntity())
+        return UserRes.from(user)
+    }
+
+    override fun getUserByGithubId(githubId: String): UserRes {
+        val user = userRepository.findByGithubId(githubId)
+            .orElseThrow { AuthException(AuthErrorCode.USER_NOT_FOUND) }
+        return UserRes.from(user)
+    }
+
+    @Transactional
+    override fun updateProfile(githubId: String, name: String?, email: String?): UserRes {
+        val user = userRepository.findByGithubId(githubId)
+            .orElseThrow { AuthException(AuthErrorCode.USER_NOT_FOUND) }
+        
+        user.updateProfile(name, email)
+        return UserRes.from(user)
+    }
+}
