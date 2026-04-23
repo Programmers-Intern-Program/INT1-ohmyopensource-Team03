e9f7262268cc8a0f062f2d0e923565624bcb481e
onethousand121 <onethousand121@gmail.com>
2026-04-23 14:22:04 +0900

    Fix: getguide, getpseudocode에 transactional 추가

--- omos/src/main/kotlin/com/back/omos/domain/analysis/service/ContextAnalyzerServiceImpl.kt
+++ omos/src/main/kotlin/com/back/omos/domain/analysis/service/ContextAnalyzerServiceImpl.kt
@@ -41,6 +41,7 @@ class ContextAnalyzerServiceImpl(
     private val objectMapper: ObjectMapper
 ) : ContextAnalyzerService {
 
+    @Transactional
     override fun getGuide(issueId: Long): GuideResponseDto {
         val issue = issueRepository.findById(issueId)
             .orElseThrow {
@@ -62,6 +63,7 @@ class ContextAnalyzerServiceImpl(
         return toGuideDto(analysisResult)
     }
 
+    @Transactional
     override fun getPseudoCode(issueId: Long): PseudoCodeResponseDto {
         val issue = issueRepository.findById(issueId)
             .orElseThrow {
